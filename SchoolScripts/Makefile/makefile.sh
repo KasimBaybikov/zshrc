2f63429da4405273ef0650dfc4043e910d59149e
KasimBaybikov <kasim.baybikov@gmail.com>
2022-07-19 14:38:49 +0300

    delete garbage and upgrade zshrc

--- SchoolScripts/Makefile/makefile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-if [[ ! -w Makefile ]]; then
-	cat /Users/rvernon/scripts/Makefile/exampleMake > $PWD/Makefile
-fi
--- SchoolScripts/go_isntall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -o /goinfre/rvernon/go.tar.gz -L https://go.dev/dl/go1.17.7.darwin-amd64.tar.gz 2>/dev/null 1>/dev/null
-tar -xf /goinfre/rvernon/go.tar.gz -C /goinfre/rvernon/ 2>/dev/null 1>/dev/null
-rm -rf /goinfre/rvernon/go_tar
-export PATH=$PATH:/goinfre/rvernon/go/bin
